22016ad557e6c319124e181330a1aedd4bf7ac68
Iron Tony <lifegamecom1@gmail.com>
2020-06-15 11:58:13 +0300

    Challenges Fragment Refactoring

--- app/src/main/java/com/example/challengeup/fragment/ChallengesFragment.java
+++ app/src/main/java/com/example/challengeup/fragment/ChallengesFragment.java
@@ -8,13 +8,13 @@ import android.view.animation.Animation;
 import android.view.animation.BounceInterpolator;
 import android.view.animation.ScaleAnimation;
 import android.widget.CompoundButton;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.navigation.Navigation;
@@ -29,12 +29,13 @@ import com.example.challengeup.ILoadable;
 import com.example.challengeup.R;
 import com.example.challengeup.backend.ChallengeEntity;
 import com.example.challengeup.backend.UserEntity;
+import com.example.challengeup.databinding.FragmentChallengesBinding;
+import com.example.challengeup.databinding.ItemChallengeBinding;
+import com.example.challengeup.dto.ChallengeDTO;
 import com.example.challengeup.request.Result;
 import com.example.challengeup.viewModel.ChallengesViewModel;
+import com.example.challengeup.viewModel.MainActivityViewModel;
 import com.example.challengeup.viewModel.factory.ChallengesFactory;
-import com.google.android.material.chip.Chip;
-import com.google.android.material.chip.ChipDrawable;
-import com.google.firebase.auth.FirebaseAuth;
 import com.volokh.danylo.hashtaghelper.HashTagHelper;
 
 import org.jetbrains.annotations.NotNull;
@@ -44,17 +45,18 @@ import java.util.List;
 
 public class ChallengesFragment extends Fragment {
 
+    private FragmentChallengesBinding mBinding;
     private ChallengesViewModel mViewModel;
+    private ScaleAnimation mScaleAnimation;
     private List<ChallengeEntity> mArrayList = new ArrayList<>();
-    private Adapter mAdapter;
-    private View view;
 
     @Override
-    public View onCreateView(LayoutInflater inflater,
+    public View onCreateView(@NotNull LayoutInflater inflater,
                              ViewGroup container,
                              Bundle savedInstanceState) {
-        view = inflater.inflate(R.layout.fragment_challenges, container, false);
-        return view;
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_challenges,
+                container, false);
+        return mBinding.getRoot();
     }
 
     @Override
@@ -66,14 +68,20 @@ public class ChallengesFragment extends Fragment {
                 appContainer.mRequestExecutor
         )).get(ChallengesViewModel.class);
 
-        RecyclerView recyclerView = view.findViewById(R.id.challenges_list);
+        mScaleAnimation = new ScaleAnimation(0.7f, 1.0f, 0.7f, 1.0f,
+                Animation.RELATIVE_TO_SELF, 0.7f,
+                Animation.RELATIVE_TO_SELF, 0.7f);
+        mScaleAnimation.setDuration(500);
+        mScaleAnimation.setInterpolator(new BounceInterpolator());
+
+        RecyclerView recyclerView = mBinding.challengesList;
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this.getActivity()));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
         recyclerView.addItemDecoration(new DividerItemDecoration(view.getContext(), LinearLayoutManager.VERTICAL));
 
-        mAdapter = new Adapter(mArrayList);
-        recyclerView.setAdapter(mAdapter);
+        Adapter adapter = new Adapter(mArrayList);
+        recyclerView.setAdapter(adapter);
 
         ILoadable loadable = (ILoadable) requireActivity();
         loadable.startLoading();
@@ -82,17 +90,18 @@ public class ChallengesFragment extends Fragment {
             if (result instanceof Result.Success) {
                 //noinspection unchecked
                 mArrayList = ((Result.Success<List<ChallengeEntity>>) result).data;
-                mAdapter.setDataset(mArrayList);
-                mAdapter.notifyItemRangeInserted(0, mArrayList.size());
+                adapter.setDataset(mArrayList);
             }
             loadable.finishLoading();
         });
-
     }
 
-    class Adapter extends RecyclerView.Adapter<Adapter.MyViewHolder> {
+    class Adapter extends RecyclerView.Adapter<Adapter.ChallengeViewHolder> {
 
         private List<ChallengeEntity> mDataset;
+        private TextView mDescription;
+        private CompoundButton likedButton;
+        private CompoundButton savedButton;
 
         public Adapter(@NonNull List<ChallengeEntity> myDataset) {
             mDataset = myDataset;
@@ -100,107 +109,31 @@ public class ChallengesFragment extends Fragment {
 
         @NotNull
         @Override
-        public Adapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View itemView = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.item_challenge, parent, false);
+        public ChallengeViewHolder onCreateViewHolder(@NotNull ViewGroup parent,
+                                                      int viewType) {
+            ItemChallengeBinding binding = DataBindingUtil.inflate(
+                    LayoutInflater.from(parent.getContext()),
+                    R.layout.item_challenge, parent, false);
 
-            return new MyViewHolder(itemView);
+            mDescription = binding.description;
+            likedButton = binding.buttonLiked;
+            savedButton = binding.buttonSave;
+
+            return new ChallengesFragment.Adapter.ChallengeViewHolder(binding);
         }
 
         @Override
-        public void onBindViewHolder(@NotNull MyViewHolder holder, int position) {
+        public void onBindViewHolder(@NotNull ChallengeViewHolder holder, int position) {
             ChallengeEntity challenge = mDataset.get(position);
 
-            // holder.thumbnail.setImageBitmap(challenge.// todo bookmarkChacked
+            ChallengeDTO challengeDTO = new ChallengeDTO();
 
-            ViewGroup chips = view.findViewById(R.id.searchArea).findViewById(R.id.chipFilter);
+            challengeDTO.setName(challenge.getName());
+            challengeDTO.setDescription(challenge.getTask());
+            challengeDTO.setLiked(String.valueOf(challenge.getLikes()));
 
-            mViewModel.getCategories(result -> {
-                if (result instanceof Result.Success) {
-                    ArrayList<String> categories = (ArrayList<String>) ((Result.Success) result).data;
-                    if (categories != null) {
-                        for (int i = 0; i < categories.size(); i++) {
-                            String category = categories.get(i);
-                            Chip chip = new Chip(getContext());
-                            chip.setChipDrawable(ChipDrawable.createFromResource(getContext(), R.xml.item_chip_filter));
-                            chip.setText(category);
-                            chip.setOnClickListener(v -> {
-                                //TODO Implement filtering logic hire
-                                Toast.makeText(getContext(), chip.getText(), Toast.LENGTH_LONG).show();
-                            });
-                            chips.addView(chip);
-//                            chips.add
-                        }
-                    }
-                }
-            });
-
-            holder.name.setText(challenge.getName());
-            holder.description.setText(challenge.getTask());
-            holder.dataLiked.setText(String.valueOf(challenge.getLikes()));
-
-            HashTagHelper mTextHashTagHelper = HashTagHelper.Creator.create(ContextCompat.getColor(getContext(), R.color.colorPrimary),
-                    hashTag -> {
-                        Toast.makeText(getContext(), hashTag, Toast.LENGTH_SHORT).show();
-                    }, '_');
-            mTextHashTagHelper.handle(holder.description);
-
-            holder.itemView.setOnClickListener(view -> {
-                ChallengesFragmentDirections.ActionChallengesToChallenge action =
-                        ChallengesFragmentDirections.actionChallengesToChallenge(challenge.getId());
-                Navigation.findNavController(view).navigate(action);
-            });
-
-            ScaleAnimation scaleAnimation =
-                    new ScaleAnimation(0.7f, 1.0f, 0.7f, 1.0f,
-                            Animation.RELATIVE_TO_SELF, 0.7f,
-                            Animation.RELATIVE_TO_SELF, 0.7f);
-            scaleAnimation.setDuration(500);
-            BounceInterpolator bounceInterpolator = new BounceInterpolator();
-            scaleAnimation.setInterpolator(bounceInterpolator);
-
-            CompoundButton likedButton =
-                    holder.itemView.findViewById(R.id.button_liked);
-            CompoundButton savedButton =
-                    holder.itemView.findViewById(R.id.buttonSave);
-
-            mViewModel.getUserByEmail(FirebaseAuth.getInstance().getCurrentUser().getEmail(), userResult -> {
-                if (userResult instanceof Result.Success) {
-                    UserEntity user = ((Result.Success<UserEntity>) userResult).data;
-                    if (user != null) {
-                        try {
-                            if (user.getLiked().contains(challenge.getId())) {
-//                            likedButton.setChecked(true);
-                                likedButton.setChecked(true);
-                            }
-                        } finally {
-                            likedButton.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-                                //animation
-                                compoundButton.startAnimation(scaleAnimation);
-                                if (isChecked) {
-                                    holder.dataLiked.setText(String.valueOf(Integer.parseInt(holder.dataLiked.getText().toString()) + 1));
-                                } else {
-                                    holder.dataLiked.setText(String.valueOf(Integer.parseInt(holder.dataLiked.getText().toString()) - 1));
-                                }
-                                mViewModel.setLiked(user, challenge, isChecked, r -> {
-                                });
-                            });
-                        }
-                        try {
-                            if (user.getSaved().contains(challenge.getId())) {
-                                savedButton.setChecked(true);
-                            }
-                        } finally {
-                            savedButton.setOnCheckedChangeListener((compoundButton, isChecked) -> {
-                                //animation
-                                compoundButton.startAnimation(scaleAnimation);
-                                mViewModel.setBookmarked(user, challenge, isChecked, r -> {
-                                });
-                            });
-                        }
-                    }
-                }
-            });
+            holder.bind(challengeDTO);
+            holder.bindThumbnail(challenge.getPhoto());
 
             mViewModel.getUserById(challenge.getCreator_id(), result -> {
                 if (result instanceof Result.Success) {
@@ -208,9 +141,35 @@ public class ChallengesFragment extends Fragment {
                     UserEntity user = ((Result.Success<UserEntity>) result).data;
                     if (user != null) {
 
-//                        Bitmap avatar = user.getPhoto();
-//                        if (avatar != null)
-//                            holder.avatar.setImageBitmap(avatar);
+                        if (user.getLiked().contains(challenge.getId()))
+                            likedButton.setChecked(true);
+                        if (user.getSaved().contains(challenge.getId()))
+                            savedButton.setChecked(true);
+
+                        if (user.getPhoto() != null)
+                            holder.bindAvatar(user.getPhoto());
+                        else
+                            holder.bindAvatar(MainActivityViewModel.DEFAULT_AVATAR_URL);
+
+                        likedButton.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+                            compoundButton.startAnimation(mScaleAnimation);
+
+                            String prevLiked = challengeDTO.getLiked();
+                            if (isChecked)
+                                challengeDTO.setLiked(String.valueOf(Integer.parseInt(prevLiked) + 1));
+                            else
+                                challengeDTO.setLiked(String.valueOf(Integer.parseInt(prevLiked) - 1));
+                            holder.bind(challengeDTO);
+
+                            mViewModel.setLiked(user, challenge, isChecked, ignored -> { });
+                        });
+
+                        savedButton.setOnCheckedChangeListener((compoundButton, isChecked) -> {
+                            compoundButton.startAnimation(mScaleAnimation);
+
+                            mViewModel.setBookmarked(user, challenge, isChecked, ignored -> { });
+                        });
+
                     }
                 }
             });
@@ -219,7 +178,8 @@ public class ChallengesFragment extends Fragment {
                 if (result instanceof Result.Success) {
                     //noinspection unchecked
                     Long num = ((Result.Success<Long>) result).data;
-                    holder.dataAccepted.setText(String.format("%s", num.toString()));
+                    challengeDTO.setAccepted(String.valueOf(num));
+                    holder.bind(challengeDTO);
                 }
             });
 
@@ -227,8 +187,23 @@ public class ChallengesFragment extends Fragment {
                 if (result instanceof Result.Success) {
                     //noinspection unchecked
                     Long num = ((Result.Success<Long>) result).data;
-                    holder.dataCompleted.setText(String.format("%s", num.toString()));
+                    challengeDTO.setCompleted(String.valueOf(num));
+                    holder.bind(challengeDTO);
                 }
+            });
+
+            HashTagHelper mTextHashTagHelper = HashTagHelper.Creator
+                    .create(
+                            ContextCompat.getColor(requireContext(), R.color.colorPrimary),
+                            hashTag -> Toast.makeText(getContext(), hashTag, Toast.LENGTH_SHORT)
+                                    .show(), '_'
+                    );
+            mTextHashTagHelper.handle(mDescription);
+
+            holder.itemView.setOnClickListener(view -> {
+                ChallengesFragmentDirections.ActionChallengesToChallenge action =
+                        ChallengesFragmentDirections.actionChallengesToChallenge(challenge.getId());
+                Navigation.findNavController(view).navigate(action);
             });
         }
 
@@ -242,21 +217,28 @@ public class ChallengesFragment extends Fragment {
             notifyDataSetChanged();
         }
 
-        public class MyViewHolder extends RecyclerView.ViewHolder {
+        public class ChallengeViewHolder extends RecyclerView.ViewHolder {
 
-            ImageView thumbnail, avatar;
-            TextView name, description, dataAccepted, dataCompleted, dataLiked;
+            private final ItemChallengeBinding mBinding;
 
-            public MyViewHolder(View itemView) {
-                super(itemView);
+            public ChallengeViewHolder(ItemChallengeBinding binding) {
+                super(binding.getRoot());
+                mBinding = binding;
+            }
 
-                thumbnail = itemView.findViewById(R.id.thumbnail);
-                avatar = itemView.findViewById(R.id.avatar);
-                name = itemView.findViewById(R.id.name);
-                description = itemView.findViewById(R.id.description);
-                dataAccepted = itemView.findViewById(R.id.dataAccepted);
-                dataCompleted = itemView.findViewById(R.id.dataCompleted);
-                dataLiked = itemView.findViewById(R.id.dataLiked);
+            public void bind(ChallengeDTO challengeDTO) {
+                mBinding.setChallenge(challengeDTO);
+                mBinding.executePendingBindings();
+            }
+
+            public void bindThumbnail(String url) {
+                mBinding.setThumbnail(url);
+                mBinding.executePendingBindings();
+            }
+
+            public void bindAvatar(String url) {
+                mBinding.setAvatar(url);
+                mBinding.executePendingBindings();
             }
         }
     }
